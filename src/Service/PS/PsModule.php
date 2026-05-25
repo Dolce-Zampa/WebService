5740486ca1708cc7cc0e07080d903f7aa8b74cfe
Marco De Felice <marco.defelice890@gmail.com>
2026-05-25 09:54:24 +0200

    hotfix

--- src/Service/PS/PsModule.php
+++ src/Service/PS/PsModule.php
@@ -3,12 +3,6 @@ declare(strict_types=1);
 
 namespace PS\Webservice\Service\PS;
 
-use PS\Webservice\Domain\Entities\FilterEntity;
-use PS\Webservice\Domain\Entities\ProductEntity;
-use PS\Webservice\Domain\Models\ProductLangTable;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Log;
-use PS\Webservice\Domain\Object\Filter;
 use PS\Webservice\Service\HttpServiceInterface;
 
 class PsModule extends PrestashopService implements PrestashopServiceInterface
