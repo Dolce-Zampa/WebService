375d33613d1fa92a84aa897fa0c5c7be521bde75
Marco De Felice <marco.defelice890@gmail.com>
2026-07-24 18:06:27 +0200

    fix

--- src/Domain/Models/User.php
+++ src/Domain/Models/User.php
@@ -6,7 +6,7 @@ namespace PS\Webservice\Domain\Models;
 class User extends PsTable
 {
 	protected $table = 'customer';
-	protected $primaryKey = 'id_user';
+	protected $primaryKey = 'id_customer';
 	public $timestamps = false;
 	protected $guarded = [];
 
